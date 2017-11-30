b772a33b5127d07095d2fb85a79a33d4792eae8f
milanchheda <milan.chheda@gmail.com>
2017-11-30 13:55:33 +0400

    Added SEO related stuff.

--- config/seotools.php
+++ config/seotools.php
@@ -7,7 +7,7 @@ return [
          */
         'defaults'       => [
             'title'        => "Learn PHP Today", // set false to total remove
-            'description'  => 'A curated top-rated video tutorials posted on YouTube & tweet snippets posted on Twitter.', // set false to total remove
+            'description'  => 'A curated list of top-rated video tutorials from YouTube & tweet snippets from Twitter.', // set false to total remove
             'separator'    => ' - ',
             'keywords'     => ['PHP', 'Laravel', 'tweet snippets', 'JavaScript', 'Learn PHP', 'PHP7'],
             'canonical'    => false, // Set null for using Url::current(), set false to total remove
@@ -30,7 +30,7 @@ return [
          */
         'defaults' => [
             'title'       => 'Learn PHP Today', // set false to total remove
-            'description' => 'A curated top-rated video tutorials posted on YouTube & tweet snippets posted on Twitter.', // set false to total remove
+            'description' => 'A curated list of top-rated video tutorials from YouTube & tweet snippets from Twitter.', // set false to total remove
             'url'         => null, // Set null for using Url::current(), set false to total remove
             'type'        => false,
             'site_name'   => false,
@@ -42,11 +42,12 @@ return [
          * The default values to be used by the twitter cards generator.
          */
         'defaults' => [
-          'card'        => 'summary',
+          'card'        => 'summary_large_image',
           'title'        => 'Learn PHP Today',
-          'description'        => 'A curated top-rated video tutorials posted on YouTube & tweet snippets posted on Twitter.',
+          'description'        => 'A curated list of top-rated video tutorials from YouTube & tweet snippets from Twitter.',
           'site'        => '@learn_php_today',
           'creator'        => '@milanchheda',
+          'image' => env('APP_URL') . '/images/homepage.png',
         ],
     ],
 ];
